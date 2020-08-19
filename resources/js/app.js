d4a0a84bdb64fe624f4c362c4adc95a5cb4794a6
tomopan <tomoya.pan66@gmail.com>
2020-08-19 15:04:43 +0900

    first-commit

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+require("./bootstrap");
+
+window.Vue = require("vue");
+import vuetify from "./vuetify";
+import router from "./router";
+import Example from "./components/ExampleComponent";
+
+const app = new Vue({
+el: "#app",
+router,
+vuetify,
+components: {
+"example-component": Example
+    }
+});
