b7a2bfdbcbff76a307337ee4f57e4fbdb81a6ae1
tsujishihori <shihori.23@i.softbank.jp>
2020-09-06 22:13:59 +0900

    [追加] 処置編集ページ / 処置編集・バリデーション（仮）

--- resources/js/components/pages/leader/EditTreatment.vue
+++ resources/js/components/pages/leader/EditTreatment.vue
@@ -1,32 +1,113 @@
 <template>
-    <!-- 仮オブジェクト -->
     <v-container class="editTreatment">
-        <v-card>
-            <v-list-item>
-                <v-list-item-content>
-                    <v-list-item-title>【リーダー】処置一覧</v-list-item-title>
-                    <v-list>
+        <v-card-text class="text-center">
+            <h1 class="headline">処置編集</h1>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-row>
+                    <v-col cols="12" class="py-0">
                         <v-text-field
+                            label="処置名"
                             v-model="editTreatment.name"
+                            color="#62ABF8"
+                            :rules="[validationRules.required]"
                         ></v-text-field>
-                        <v-text-field
+                    </v-col>
+                    <v-col cols="12" class="py-0">
+                        <v-select
                             v-model="editTreatment.time_required"
-                        ></v-text-field>
-                        <v-text-field
+                            :items="timeRequired"
+                            label="処置時間"
+                            color="#62ABF8"
+                            clearable
+                            :rules="[validationRules.required]"
+                        ></v-select>
+                    </v-col>
+                    <v-col cols="12" class="py-0">
+                        <header class="labelFlag text-left">優先</header>
+                        <v-switch
                             v-model="editTreatment.required_flg"
-                        ></v-text-field>
-                        <v-btn
-                            class="ma-2"
-                            outlined
-                            color="pink lighten-1"
-                            @click="Update(editTreatment.id)"
-                        >
-                            変更
-                        </v-btn>
-                    </v-list>
-                </v-list-item-content>
-            </v-list-item>
-        </v-card>
+                            :persistent-hint="true"
+                            :true-value="0"
+                            :false-value="1"
+                            hint="時間不可の場合チェックをつけてください"
+                            inset
+                            color="#62ABF8"
+                            class="mt-2"
+                        ></v-switch>
+                    </v-col>
+                    <v-btn
+                        class="mx-auto my-4 px-12 py-4 submit_btn"
+                        rounded
+                        dark
+                        color="#62ABF8"
+                        type="submit"
+                        @click="updateTreatmentData()"
+                    >
+                        変更
+                    </v-btn>
+                </v-row>
+            </v-form>
+        </v-card-text>
+
+        <v-dialog v-model="isDialogOpen.successDialog" width="400">
+            <v-card>
+                <v-card-title
+                    class="headline lighten-2 text--secondary"
+                    color="#62ABF8"
+                    primary-title
+                    >完了</v-card-title
+                >
+                <v-card-text
+                    >下記内容で処置登録が完了しました！
+                    <p class="successMessage mb-0">
+                        処置名：{{ editTreatment.name }}
+                    </p>
+                    <p class="successMessage mb-0">
+                        患者名：{{ editTreatment.time_required }}分
+                    </p>
+                    <p class="successMessage mb-0">
+                        必須：{{ selectedrequiredFlg }}
+                    </p>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="#62ABF8"
+                        text
+                        @click="closeDialog('successDialog')"
+                        >閉じる</v-btn
+                    >
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <v-dialog v-model="isDialogOpen.errorDialog" width="400">
+            <v-card>
+                <v-card-title
+                    class="headline lighten-2 text--secondary"
+                    primary-title
+                    >エラー</v-card-title
+                >
+                <v-card-text>
+                    <p
+                        v-for="(message, index) in axiosErrorMessages"
+                        :key="index"
+                        class="errorMessage"
+                    >
+                        ・{{ axiosErrorMessages[index] }}
+                    </p>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="#62ABF8"
+                        text
+                        @click="closeDialog('errorDialog')"
+                        >閉じる</v-btn
+                    >
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 <script>
@@ -36,78 +117,134 @@
 export default {
     components: {},
     data: () => ({
-        treatments: [],
+        validationRules: {
+            required: value => !!value || "入力必須です。",
+            nameCounter: value =>
+                (value || "").length <= 20 ||
+                "処置名は20字以下で入力してください。"
+        },
+        valid: true,
+        isDialogOpen: {
+            successDialog: false,
+            errorDialog: false
+        },
+        setTreatmentRequiredFlag: true,
         editTreatment: {
             name: "",
             time_required: "",
             required_flg: ""
-        }
+        },
+        timeRequired: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60],
+        selectedrequiredFlg: true,
+        axiosErrorMessages: []
     }),
+    created() {
+        this.getTreatmentData(this.$route.params.treatmentId);
+    },
     methods: {
-        fetchTreatment: function() {
+        // 更新する処置情報取得
+        getTreatmentData: function(treatmentId) {
             axios
-                .get("/api/treatments/get/all")
+                .get("/api/treatments/get/" + treatmentId)
                 .then(res => {
                     console.log("status:", res.status);
                     console.log("body:", res.data);
-                    this.treatments = res.data;
+
+                    const fetchTreatmentData = res.data;
+                    this.editTreatment = fetchTreatmentData;
+                    const fetchRequiredFlag = fetchTreatmentData.required_flg;
+                    console.log(this.setTreatmentRequiredFlag);
                 })
                 .catch(err => {
                     console.log("err:", err);
                 });
         },
-        //処置削除
-        Delete: function(treatmentId) {
-            if (confirm("削除してよろしいでしょうか?"))
+        // formのデータを定義
+        setTreatmentData() {
+            let formData = new FormData();
+
+            Object.keys(this.editTreatment).forEach(key => {
+                if (this.editTreatment[key]) {
+                    console.log(key, this.editTreatment[key]);
+                    formData.append(key, this.editTreatment[key]);
+                }
+            });
+            formData.append("required_flg", this.editTreatment.required_flg);
+            return formData;
+        },
+        //　サーバー側からのエラーを定義
+        setAxiosErrorData(err) {
+            const axiosErrorRes = err.response.data;
+            let axiosErrorMessageArray = [];
+
+            if (axiosErrorRes.errors) {
+                const axiosvalidationErrorRes = axiosErrorRes.errors;
+
+                axiosErrorMessageArray = Object.keys(
+                    axiosvalidationErrorRes
+                ).map(dataField => {
+                    return axiosvalidationErrorRes[dataField][0];
+                });
+            } else {
+                axiosErrorMessageArray.push(
+                    "回答が送信されませんでした。再度送信してください。"
+                );
+            }
+            this.axiosErrorMessages = axiosErrorMessageArray;
+            console.log(this.axiosErrorMessages);
+            this.isDialogOpen.errorDialog = true;
+        },
+        // 処置情報更新
+        updateTreatmentData: function() {
+            //入力値のエラーを確認
+            if (this.$refs.form.validate()) {
+                const formData = this.setTreatmentData();
+                var config = {
+                    headers: {
+                        "content-type": "multipart/form-data"
+                    }
+                };
                 axios
-                    .delete("/api/treatments/delete", {
-                        data: { id: treatmentId }
-                    })
+                    .post(
+                        "/api/treatments/update/" +
+                            this.$route.params.treatmentId,
+                        formData,
+                        config
+                    )
                     .then(res => {
                         console.log("status:", res.status);
                         console.log("body:", res.data);
-                        this.treatments = res.data;
+                        const updateTreatment = res.data;
+                        const updateTreatmentRequiredFlg =
+                            updateTreatment.required_flg;
+                        this.editTreatment = updateTreatment;
+                        if (updateTreatmentRequiredFlg == 0) {
+                            this.selectedrequiredFlg = "必須";
+                        } else {
+                            this.selectedrequiredFlg = "必須ではない";
+                        }
+                        this.isDialogOpen.successDialog = true;
                     })
                     .catch(err => {
                         console.log("err:", err);
+                        this.setAxiosErrorData(err);
                     });
+            } else {
+                console.log("エラーがあるよ！");
+            }
         },
-
-        // 更新する処置情報取得
-        Edit: function(treatmentId) {
-            axios
-                .get("/api/treatments/get/" + treatmentId, {})
-                .then(res => {
-                    console.log("status:", res.status);
-                    console.log("body:", res.data);
-                    this.editTreatment = res.data;
-                })
-                .catch(err => {
-                    console.log("err:", err);
-                });
-        },
-        // 処置情報更新
-        Update: function(editTreatmentId) {
-            axios
-                .post("/api/treatments/update/" + editTreatmentId, {
-                    id: editTreatmentId,
-                    treatment: this.editTreatment,
-                    name: this.editTreatment.name,
-                    time_required: this.editTreatment.time_required,
-                    required_flg: this.editTreatment.required_flg
-                })
-                .then(res => {
-                    console.log("status:", res.status);
-                    console.log("body:", res.data);
-                    this.treatment = res.data;
-                })
-                .catch(err => {
-                    console.log("err:", err);
-                });
+        //　ダイアログを閉じる
+        closeDialog(dialogName) {
+            this.isDialogOpen[dialogName] = false;
+            if (dialogName == "successDialog") {
+                const transitionDestinationObj = {
+                    name: "TreatmentList"
+                };
+                this.treatment = "";
+                this.$refs.form.resetValidation();
+                this.$router.push(transitionDestinationObj);
+            }
         }
-    },
-    created() {
-        this.fetchTreatment();
     }
 };
 </script>
