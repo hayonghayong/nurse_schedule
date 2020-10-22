76fd1f2ec0ba0a7cd649da472fedbd251dd6e9fe
tomopan <tomoya.pan66@gmail.com>
2020-10-23 01:29:11 +0900

    https設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,12 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {        
+    {       
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
+        // herokuデプロイ時の文字数制限
         Schema::defaultStringLength(191);
         //  管理画面用のクッキー名称、セッションテーブル名を変更する < いったんコメントアウト
         // $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
