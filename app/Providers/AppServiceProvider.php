52c03205c6796574909699c1ef499a3f6f259738
tomopan <tomoya.pan66@gmail.com>
2020-10-23 15:41:12 +0900

    heroku_reset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {        
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
         //  管理画面用のクッキー名称、セッションテーブル名を変更する < いったんコメントアウト
         // $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
         // if (strpos($uri, '/admin/') === 0 || $uri === '/admin') {
